e6a1e73ea2b0ff4a90dad29746328efc1d7d07a8
yuri <yuri.golubovski091@gmail.com>
2025-03-17 19:57:49 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { AccessToken } = require('livekit-server-sdk')
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+app.post('/api/sandbox/connection-details', async (req, res) => {
+	const roomName = req.query.roomName || "defaultRoom";
+	const participantName = req.query.participantName || "defaultParticipant";
+	
+	const at = new AccessToken(process.env.LIVEKIT_API_KEY, process.env.LIVEKIT_API_SECRET, 
+	{
+		identity: participantName,
+		ttl: 3600
+	});
+	
+	at.addGrant({ roomJoin: true, room: roomName});
+	
+	const token = await at.toJwt();
+	
+	const responseJson = {
+		participantToken: token,
+		roomName: roomName,
+		serverUrl: process.env.LIVEKIT_SERVER_URL,
+		participantName: participantName
+	};
+	
+	res.json(responseJson);
+});
+
+
+app.listen(port, () => {
+	console.log('Token server is listening on port ' + port);
+});
